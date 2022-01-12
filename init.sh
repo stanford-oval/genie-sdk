1b648ffe3d09352fafa8381a789579a13d30a5a1
Silei Xu <ray.slxu@gmail.com>
2022-01-12 12:19:28 -0800

    Add script to initialize a skill

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+. ./lib.sh
+parse_args "$0" "skill_name" "$@"
+
+set -x
+
+if ! test -d workdir ; then
+	node --experimental_worker ./genie-toolkit/dist/tool/genie.js init-project \
+	  --developer-key 88c03add145ad3a3aa4074ffa828be5a391625f9d4e1d0b034b445f18c595656  \
+	  workdir
+fi
+  
+if ! test -d workdir/${skill_name} ; then
+	pushd workdir >/dev/null
+    node --experimental_worker ../genie-toolkit/dist/tool/genie.js init-device \
+	  ${skill_name}
+	popd >/dev/null
+fi
+
+if ! test -d devices/${skill_name} ; then
+	mkdir -p devices
+	ln -s ${PWD}/workdir/${skill_name} ./devices/${skill_name}
+fi
