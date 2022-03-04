77e263d3138cfe9c2844e4427ca148f6533904bd
Silei Xu <ray.slxu@gmail.com>
2022-03-03 16:44:43 -0800

    Add the option to init project/device for single turn assistant

--- init.sh
+++ init.sh
@@ -4,20 +4,22 @@ set -e
 set -o pipefail
 
 . ./lib.sh
-parse_args "$0" "skill_name" "$@"
+parse_args "$0" "skill_name type" "$@"
 
 set -x
 
 if ! test -d workdir ; then
 	node --experimental_worker ./genie-toolkit/dist/tool/genie.js init-project \
 	  --developer-key 88c03add145ad3a3aa4074ffa828be5a391625f9d4e1d0b034b445f18c595656  \
+	  --type ${type} \
 	  workdir
 fi
   
 if ! test -d workdir/${skill_name} ; then
 	pushd workdir >/dev/null
     node --experimental_worker ../genie-toolkit/dist/tool/genie.js init-device \
-	  ${skill_name}
+	  ${skill_name} \
+      --type ${type}
 	popd >/dev/null
 fi
 
