781df8028faf5917967b993d8b80be2b699ae2a2
Silei Xu <rayslxu@users.noreply.github.com>
2022-05-03 16:19:35 -0700

    Update genie version

--- install.sh
+++ install.sh
@@ -86,12 +86,9 @@ python -m spacy download en_core_web_sm
 if ! test -d genie-toolkit ; then
 	git clone https://github.com/stanford-oval/genie-toolkit
 	pushd genie-toolkit >/dev/null
-	git checkout wip/fix-make
 	npm ci
 	popd >/dev/null
 fi
-
-
 
 if ! test -d genie-server ; then
 	git clone https://github.com/stanford-oval/genie-server
